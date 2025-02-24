cc178c16ffccf4c7d863fdfa02707203b690752a
SatsumaSegment <gregduke92@gmail.com>
2025-02-24 17:58:37 +0000

    changed direct line variables to use underscore

--- helpers/get_tool_envs.py
+++ helpers/get_tool_envs.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 def load_envs():
     load_dotenv()
 
-    tool_prefix = "DL-AZ-"
+    tool_prefix = "DL_AZ_"
 
     matching_tools = {key: os.environ[key] for key in os.environ if key.startswith(tool_prefix)}
 
     tools_with_descriptions = {}
 
     for key, value in matching_tools.items():
-        if key.endswith("-DESCRIPTION"):
-            base_tool = key.replace("-DESCRIPTION", "")
+        if key.endswith("_DESCRIPTION"):
+            base_tool = key.replace("_DESCRIPTION", "")
             if base_tool in matching_tools:
                 tools_with_descriptions[base_tool] = value
 
